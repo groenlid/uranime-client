8603c46079b5e669b31ffe1d3aec672af1da4127
Mats Groenlid <groenlid@gmail.com>
2013-05-23 23:49:45 +0200

    cropped the loading gif , added some inline-search to the navigation-bar

--- app/modules/search/searchFormView.js
+++ app/modules/search/searchFormView.js
@@ -26,14 +26,45 @@ App.SearchFormView = Ember.ContainerView.extend({
         templateName: "search/searchForm",
 
         classNameBindings:['show::hidden'],
+        
+        inlinesearch: function(){
+                var parent = this.get('parentView'),
+                    hidden = !parent.get('showResult'),
+                    value = this.get('value'),
+                    result;
+                
+                if(hidden) return Ember.debug('The search was activated with enter, so skipping inline-search.');
+                
+                result = App.Anime.find({title:value});
+                this.set('result', result);
+        },
 
         searchField: Ember.TextField.extend({
+
+            valueBinding: 'parentView.value',
+
+            // Start the timer
+            onValueChange: function(){
+                var that = this,
+                    parent = this.get('parentView'),
+                    scheduled = this.get('scheduled');
+                
+                // Cancel any planned runs
+                Ember.run.cancel(scheduled);
+
+                // Make some new plans
+                scheduled = Ember.run.later(parent,parent.get('inlinesearch'),1500);
+
+                this.set('scheduled', scheduled);
+            }.observes('value'),
+
             insertNewline: function(){
                 var val = this.get('value');
                 this.get('controller').send('search', {query:val});
                 this.get('parentView.parentView').set('showResult', false);
             }
         })
+
     }),
 
 });
--- dependencies/compiled/templates.js
+++ dependencies/compiled/templates.js
@@ -1109,8 +1109,57 @@ function program1(depth0,data) {
 Ember.TEMPLATES["search/searchForm"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Ember.Handlebars.helpers; data = data || {};
-  var buffer = '', hashTypes, escapeExpression=this.escapeExpression;
+  var buffer = '', stack1, hashTypes, escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
 
+function program1(depth0,data) {
+  
+  var buffer = '', stack1, hashTypes;
+  data.buffer.push("\n                    ");
+  hashTypes = {};
+  stack1 = helpers['if'].call(depth0, "isLoaded", {hash:{},inverse:self.program(6, program6, data),fn:self.program(2, program2, data),contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n                ");
+  return buffer;
+  }
+function program2(depth0,data) {
+  
+  var buffer = '', stack1, hashTypes;
+  data.buffer.push("\n                        ");
+  hashTypes = {};
+  stack1 = helpers.each.call(depth0, "row", "in", "", {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n                    ");
+  return buffer;
+  }
+function program3(depth0,data) {
+  
+  var buffer = '', stack1, stack2, hashTypes, options;
+  data.buffer.push("\n                                ");
+  hashTypes = {'tagName': "STRING",'class': "STRING"};
+  options = {hash:{
+    'tagName': ("div"),
+    'class': ("row")
+  },inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0,depth0],types:["ID","ID"],hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "anime.description", "row", options) : helperMissing.call(depth0, "linkTo", "anime.description", "row", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n                        ");
+  return buffer;
+  }
+function program4(depth0,data) {
+  
+  var buffer = '', hashTypes;
+  data.buffer.push("\n                                        ");
+  hashTypes = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "row.title", {hash:{},contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data})));
+  data.buffer.push("               \n                                ");
+  return buffer;
+  }
+
+function program6(depth0,data) {
+  
+  
+  data.buffer.push("\n                        <div class=\"row text-align-center\">\n                                <span class=\"loading\"></span>\n                        </div>\n                    ");
+  }
 
   data.buffer.push("<div class=\"popover bottom\" id=\"notifications\" style=\"width:250px;display: block;\" data-bindattr-1=\"1\">\n        <div class=\"arrow\"></div>\n        <div class=\"popover-inner\">\n            <div class=\"popover-title\">\n                ");
   hashTypes = {'placeholder': "STRING",'valueBinding': "STRING"};
@@ -1118,7 +1167,11 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
     'placeholder': ("Search for anime"),
     'valueBinding': ("view.value")
   },contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data})));
-  data.buffer.push("\n            </div>\n            <!--<div class=\"popover-content\">\n                <div class=\"row\">\n                    Should be some magic here..\n                </div>\n                \n            </div>-->\n        </div>\n    </div>");
+  data.buffer.push("\n            </div>\n            <div class=\"popover-content searchresult\">\n                ");
+  hashTypes = {};
+  stack1 = helpers['with'].call(depth0, "view.result", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n            </div>\n        </div>\n    </div>\n");
   return buffer;
   
 });
